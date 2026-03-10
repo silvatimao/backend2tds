eaaf2999d363b2e84df33970b906820cb5c6bb8e
silvatimao <s.rafaela31@escola.pr.gov.br>
2026-03-10 11:43:44 -0300

    Add files via upload

---
+++ endereco.js
@@ -0,0 +1,8 @@
+console.log("Endereço Completo");
+let rua = "rua das flores";
+let numero = 489;
+let cidade = "curitiba";
+
+let endereco = rua +  "," + numero + "-" + cidade;
+//console.log(endereco);
+console.log("O endereço coreto é:" +endereco);
