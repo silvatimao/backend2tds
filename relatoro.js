bca85b39ec3c10b71cdcba061044853474a84d08
silvatimao <s.rafaela31@escola.pr.gov.br>
2026-03-11 08:51:08 -0300

    Add files via upload

---
+++ relatoro.js
@@ -0,0 +1,9 @@
+//relatorio e notas
+let aluno = "matheus";
+let nota1 = 8.5;
+let nota2 = 5.0;
+
+let media = (nota1 + nota2) / 2;
+let relatorio = "Aluno:"+ aluno  + "notas" +nota1 + "e" + nota2 | "Media de notas";
+console.log("relatorio de Notas");
+console.log(relatorio);
