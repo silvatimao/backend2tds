97e6048b57ca510dad8141fe727b9ad1068c2275
silvatimao <s.rafaela31@escola.pr.gov.br>
2026-05-18 13:42:53 +0000

    commit

---
+++ desafio.js
@@ -0,0 +1,12 @@
+//Declarar 5 variáveis para representar o perfil de um jogador
+
+// fazer uma saida de forma inerpolada (`${}`)
+
+const nome = "Yamal";
+const idade = 18;
+const posicao = "Ponta-direita";
+const time = "Barcelona";
+const altura =  1.80;
+const pesoEmKilos= 72;
+
+console.log(`O jogador ${nome}, segue atuando como {posicao} no time no time espanhol ${time}.e com apenas ${idade} anos de idade, ja se encontra em tamanho adulto com ${altura} de altura e ${pesoEmkilos}quilos.`)
